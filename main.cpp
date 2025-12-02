c4ed3abf2bb6905fee8a0d36051e79d2f6963692
prblmtc11 <prblmtc11@gmail.com>
2025-12-02 09:41:48 +0300

    new loll

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+namespace loll {
+    struct p_t { int x, y;};
+    struct f_t (p_t aa,bb;};
+  bool operator==(p_t a, p_t b);
+  bool operator!=(p_t a, p_t b);
+}
+
+int main() {
+    using namespace loll;
+    p_t a{1, 1}, b{0, 1};
+    std::cout << a == b << "\n";
+}
+bool loll::operator==(p_t a, p_t b){
+    return a.x == b.x && a.y == b.y;
+}
+bool loll::operator!=(p_t a, p_t b){
+    return a.x == b.x && a.y == b.y;
+}
+
+
